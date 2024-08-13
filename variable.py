db04b89ead62afd144fb81e76eb0f0b6d3b13c03
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-08-13 19:30:45 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,57 @@
+x = 10 
+print(x)
+
+y = 10.0
+print(y)
+
+z = 3+4j
+print(z)
+
+name = "krishna"
+print(name)
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+coordinates = (10, 20)
+print(coordinates)
+
+numbers = {1,2,3,4}
+print(numbers)
+
+
+digits = frozenset([1,2,3])
+print(digits)
+
+is_student = True
+print(is_student)
+
+value = None
+print(value)
+
+person = {
+    "name": "subbu",
+    "age": "20",
+    "location": "AP"
+}
+
+print(person)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
