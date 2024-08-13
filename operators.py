7d3f20789072c2f1c36ff830c94d4af6d0ee9ad3
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-08-13 19:56:58 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,93 @@
+# operators
+
+# 1. Arithmatic Operators
+
+a = 10
+b = 3
+
+print(a+b) #addition method
+print(a-b) # substraction
+print(a*b) # multiplication
+print(a/b) #division
+print(a%b) # modulus
+print(a**b) # power
+print(a//b) # floor division
+
+# 1. relation operators (comparison)
+
+a = 10
+b = 20
+
+print(a == b)
+print(a!=b)
+print(a > b)
+print(a<b)
+print(a >= b)
+print(a >= b)
+
+
+# 3. Logical Operators
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+# 4.bitwise opertors
+
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a << b)
+print(a >> b)
+
+
+# 5. assignment operators
+
+a = 5
+
+a //= 3
+print(a)
+
+# 6. identify operators
+
+a = [1,2,3]
+b = [1,2,3]
+
+c = a
+
+print(a is b)
+print(a is c)
+print(a is not b)
+
+# 7. membership operators
+
+my_list = [1,2,3,4,5]
+
+print(3 in my_list)
+print(6 not in my_list)
+print(4 not in my_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
