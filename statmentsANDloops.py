7377b796e5219b40a50ed2ce60a61f67262001af
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-08-14 20:04:22 +0530

    Create statmentsANDloops.py

---
+++ statmentsANDloops.py
@@ -0,0 +1,89 @@
+# 1. for Loops
+
+# for variable_name in sequence:
+    
+fruits = ["apple", "banana", "cherry"]
+for X in fruits:
+    print(X)
+    
+Number = [10, 20, 30, 40]
+for Y in Number:
+    sum = Y+1
+    print(sum)
+
+# 2. while Loop
+
+# while condition:
+    
+i = 1
+while i <= 2:
+    print(i)
+    i += 1
+
+# 3. if statement :
+
+# if condition:
+    
+# else:
+    
+
+num = 5
+if num > 10:
+    print("the number is greater than 10")
+else: 
+    print("the number is less than 10")
+    
+    
+# elif statment:
+
+
+num = 7
+if num > 10:
+    print("numner is greater than 10")
+elif num == 7:
+    print("the number is 7")
+else: 
+    print("number is less than 10 and not equal to 7")
+ 
+# loops with control flow problem
+X = (1,2,3,4,5,6,7,8,9,10,11)
+ind:(0,1,2,3,4,5,6,7,8,9,10)
+
+for i in range(1, 11):
+    #print(i)
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
