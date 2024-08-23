3831d5094032384985592a169307411f55db7b60
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-08-23 19:29:50 +0530

    Create listtask.py

---
+++ listtask.py
@@ -0,0 +1,15 @@
+# List Task
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits) 
+
+fruits.insert(3, "blueberry")
+print(fruits) 
+
+fruits.remove("banana")
+print(fruits)
+
+X = fruits.pop()
+print(X)
+print(fruits)
