81a540f7eea63b134d3029af3883d5d57d5fd74b
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2024-08-23 19:55:29 +0530

    Create DictionaryTask.py

---
+++ DictionaryTask.py
@@ -0,0 +1,43 @@
+# dictionary
+my_dict = {
+    # key : value
+     "name": "subbu",
+     "age" : "23",
+     "city": "Nellore"
+}
+
+dict (
+    name="subbu",
+    age="24",
+    city="Nellore"
+    )
+
+print(my_dict["name"])
+print(my_dict["age"])
+
+# using get methods
+X = my_dict.get("city")
+print(X)
+Y = my_dict.get("A")
+print(Y, "Not Found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
